9ea7f754d2c8632b15918287fe6a6c42a60ea726
Charlie Cuddy <charliecuddy4@gmail.com>
2016-10-23 21:14:53 +0000

    minor css changes to center buttons

--- script.js
+++ script.js
@@ -8,15 +8,15 @@ var view = {
 
 var handlers = {
     twoSec: function() { setTimeout(function() {
-        document.write("You clicked the button 2 seconds ago </br> <a href='index.html'>back</a>");
+        document.write("You clicked the button 2 seconds ago </br> <a href='index.html'>back</a> </br>");
     }, 2000)},
     
     fiveSec: function() { setTimeout(function() {
-        document.write("You clicked the button 5 seconds ago </br> <a href='index.html'>back</a>");
+        document.write("You clicked the button 5 seconds ago </br> <a href='index.html'>back</a> </br>");
     }, 5000)},
     
     EightSec: function() { setTimeout(function() {
-        document.write("You clicked the button 8 seconds ago </br> <a href='index.html'>back</a>");
+        document.write("You clicked the button 8 seconds ago </br> <a href='index.html'>back</a> </br>");
     }, 8000)}
 }
 
